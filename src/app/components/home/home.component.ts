8e8918cf36764b5dbb641ee7614b53d72e8189fa
Nicolas Campos <nicolascampos611@gmail.com>
2025-01-23 14:01:37 -0300

    Criado um novo componente 'envia-formulario.service' no service, e inserido informação a ser enviada ao backend quando clicar no botão

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,22 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { EnviaFormularioService } from '../../services/envia-formulario.service';
+import { inject } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  private enviaFormularioService = inject(EnviaFormularioService);
     name = "Nicolas";
     idButton = "botao";
     deveMostrarTitulo = true;
+    listItems = ["um", "dois", "tres"]
   
 // atualizaBooleano(valor: boolean){
 //    this.meuBooleano = valor;
 // }
 
-submit(event: any){
-  console.log(event)
-}
-
+submit(){
+  this.enviaFormularioService.enviaInformacaoParaBackend("enviando informação")
+  }
 }
